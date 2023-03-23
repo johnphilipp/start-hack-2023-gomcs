2ab21a4daeb47f526497ef9a37c0ddeba939be4e
dfalk <damian.falk@student.unisg.ch>
2023-03-23 12:15:07 +0100

    activitysegdoc

--- backend/src/main/java/eu/starthack/gomcsbackend/repository/TimelineDocument.java
+++ backend/src/main/java/eu/starthack/gomcsbackend/repository/TimelineDocument.java
@@ -1,9 +1,9 @@
 package eu.starthack.gomcsbackend.repository;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "timeline")
 public class TimelineDocument {
-   
 
 }
